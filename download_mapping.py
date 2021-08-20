1720f52b4ee3bffd81e800b84957e037c0ebecf3
Neel Guha <neelguha@gmail.com>
2021-08-19 23:35:34 -0700

    adding SCOTUS oral transcripts

--- download_mapping.py
+++ download_mapping.py
@@ -4,5 +4,11 @@ DOWNLOAD_MAPPING = {
             "train" : todo,
             "validation" : todo
         }
+    },
+    "scotus_oral" : {
+        "urls" :{
+            "train" : "https://drive.google.com/file/d/1dCF_pZ501Rb0Ii7LcrxJapmFvhaj2bG-/view?usp=sharing",
+            "validation" : "https://drive.google.com/file/d/1Ksm7gqQfN2eQavuCdVgAN0jE_zfJURH-/view?usp=sharing"
+        }
     }
 }
