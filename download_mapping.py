f934dbf4639679b0fac505fa6625929c11728a62
Peter Henderson <peter.henderson@mail.mcgill.ca>
2021-08-21 18:42:20 -0700

    Add olc and stanford bar outlines

--- download_mapping.py
+++ download_mapping.py
@@ -1,14 +1,26 @@
 DOWNLOAD_MAPPING = {
     "case_law" : {
         "urls" :{
-            "train" : todo,
-            "validation" : todo
+            "train" : "N/A",
+            "validation" : "https://github.com/reglab/casehold/blob/main/case_ids/task_ids.json"
         }
-    }
+    },
     "CFR" : {
         "urls" :{
             "train" : "https://drive.google.com/file/d/1NailqBHKoLNdy6anYoVAQZXFwotv3hsM/view?usp=sharing",
             "validation" : "https://drive.google.com/file/d/1-Ar_WPCTvwBgW0m-mcoDhA_AnEbyOHIw/view?usp=sharing"
+        }
+    },
+    "stanford_bar_exam_outlines" : {
+        "urls" : {
+            "train" : "https://drive.google.com/file/d/1kqKwQxCt45ccbtrBrkI-T3az_Id8cLTr/view?usp=sharing",
+            "validation" : "https://drive.google.com/file/d/1si8NrY-VS-9PHaTBCVappQbwKr3DWv_0/view?usp=sharing"
+        }
+    },
+    "olc_memos" : {
+        "urls" : {
+            "train" : "https://drive.google.com/file/d/12FP1vvMoCsHNA-Cs-wupZbX2gJC1Zv8c/view?usp=sharing",
+            "validation" : "https://drive.google.com/file/d/1zzwAcgzeDHC3hS6vv7U_coWQfpW96npF/view?usp=sharing"
         }
     }
 }
