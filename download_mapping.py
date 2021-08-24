367b3c3c871e01d43db40331ff1a4c014c23f2c0
Neel Guha <neelguha@gmail.com>
2021-08-23 22:56:40 -0700

    download mappings

--- download_mapping.py
+++ download_mapping.py
@@ -4,5 +4,11 @@ DOWNLOAD_MAPPING = {
             "train" : todo,
             "validation" : todo
         }
+    },
+    "euro_parl" : {
+        "urls" :{
+            "train" : "https://drive.google.com/file/d/1BK2iRz15dAUo-JsYHeluflABTCO3pLLW/view?usp=sharing",
+            "validation" : "https://drive.google.com/file/d/1bCu3zgah8poPKwv9VXwY2klxz6Dr-Tbp/view?usp=sharing"
+        }
     }
 }
