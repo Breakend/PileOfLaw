7208ba6821da222df68c73eb373163e73461a01e
Lucia Zheng <zlucia@stanford.edu>
2021-08-30 16:07:35 -0700

    Add BVA opinions

--- download_mapping.py
+++ download_mapping.py
@@ -4,5 +4,11 @@ DOWNLOAD_MAPPING = {
             "train" : todo,
             "validation" : todo
         }
+    },
+    "bva" : {
+        "urls" :{
+            "train": "https://drive.google.com/file/d/14SXhyaojMwXjPlcb1GXpK3E6u-abL0Tz/view?usp=sharing",
+            "validation": "https://drive.google.com/file/d/1admnbREtKOzHVe6o4DyqWiJiqvRzJQ3h/view?usp=sharing"
+        }
     }
 }
