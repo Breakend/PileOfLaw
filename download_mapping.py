17052fabb1c2f19750f7d09d2e2162e9461f11a9
Neel Guha <neelguha@gmail.com>
2021-10-03 11:44:19 -0700

    Adding URLs for atticus contracts

--- download_mapping.py
+++ download_mapping.py
@@ -46,5 +46,11 @@ DOWNLOAD_MAPPING = {
             "train" : "https://drive.google.com/file/d/1s59DllnfJxYjz89M1RWKgh7zmMNyMQ5u/",
             "validation" : "https://drive.google.com/file/d/1azhvkZu8d-kX83XApgbzPnhnStKMkxne"
         }
+    },
+    "atticus_contracts" : {
+        "urls" :{
+            "train" : "https://drive.google.com/file/d/1_4qSNBNbD1s3tuB9XbbPPClXk0y_Rq15/view?usp=sharing",
+            "validation" : "https://drive.google.com/file/d/1H4ac_0VjaIcpS6gbwCYovGxanUkUSd7L/view?usp=sharing"
+        }
     }
 }
