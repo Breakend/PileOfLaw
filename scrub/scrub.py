be83714eabe4279922cbadc84f62fe04590c6197
Lucia Zheng <zlucia@stanford.edu>
2021-12-01 12:34:23 -0800

    Modify scrub script to generate and save detailed filth statistics for scrubbed documents

--- scrub/scrub.py
+++ scrub/scrub.py
@@ -1,10 +1,11 @@
 import glob
 import os
 import json
-import lzma as xz
-import tarfile
+try:
+	import lzma as xz
+except ImportError:
+	import pylzma as xz
 import scrubadub
-import pickle
 from tqdm import tqdm
 
 # DATA DIRS
@@ -12,45 +13,107 @@ IN_DIR = 'data'
 OUT_DIR = 'data_scrubbed'
 FILTH_DIR = 'filth'
 
+# remaining 
+TO_SCRUB = ['train.atticus_contracts.jsonl.xz',
+			'validation.atticus_contracts.jsonl.xz',
+			'train.bva.jsonl.xz',
+			'validation.bva.jsonl.xz',
+			'train.congressional_hearings.xz',
+			'validation.congressional_hearings.xz',
+			'train.courtlistenerdocketentries.xz',
+			'validation.courtlistenerdocketentries.xz',
+			'train.courtlisteneropinions.xz',
+			'validation.courtlisteneropinions.xz',
+			'train.edgar.jsonl.xz',
+			'validation.edgar.jsonl.xz',
+			]
+
+
 def scrub(filepath, scrubber):
-		"""This function scrubs the data file of sensitive information."""
+		"""This function scrubs the data file of sensitive information.
+			Returns
+				- A list of the documents in the data file, with text scrubbed
+				- A dict of the file filth data
+		"""
 		print(f"Scrub {filepath}")
 
-		docs_scrubbed = []
-		docs_filth = []
-
+		print(f"Read {filepath}")
+		lines = []
 		with xz.open(filepath, mode='rb') as f:
-			print(f"Read {filepath}")
-			lines = []
 			while True:
 				try:
-					line = f.readline()
-					lines.append(line)
+					line = f.readline().decode('utf-8')
 					if line == "":
 						break
+					lines.append(line)
 				except:
 					print("corrupted line")
 					break
 
 
-			print(f"Clean {filepath}")
-			for line in tqdm(lines):
-				if line is not None and line != "":
-					doc = json.loads(line)
-					text = doc["text"]
+		print(f"Clean {filepath}")
+		docs_scrubbed = []
+		docs_filth_data = []
 
-					# Detect filth
-					filth = list(scrubber.iter_filth(text))
-					filth = [(item.detector_name, item.text) for item in filth]
+		filth_count = 0
+		word_count = 0
+		filth_doc_count = 0
 
-					# Clean
-					cleaned_text = scrubber.clean(text)
-					doc["text"] = cleaned_text
-				
-					docs_scrubbed.append(doc)
-					docs_filth.append(filth)
+		for line in tqdm(lines):
+			if line is not None and line != "":
+				doc = json.loads(line)
+				doc_text = doc["text"]
+				doc_word_count = len(doc_text.split())
+				word_count += doc_word_count
 
-		return docs_scrubbed, docs_filth
+				# Detect filth
+				filth_objs = list(scrubber.iter_filth(doc_text))
+				doc_filth_count = len(filth_objs)
+				filth_count += doc_filth_count
+				if doc_filth_count > 0:
+					filth_doc_count += 1
+
+				doc_filth = []
+				for filth_obj in filth_objs:
+					filth = {}
+					if isinstance(filth_obj, scrubadub.filth.base.MergedFilth): # filth_obj is MergedFilth
+						types = []
+						texts = []
+						for filth_obj_i in filth_obj.filths:
+							types.append(filth_obj_i.detector_name)
+							texts.append(filth_obj_i.text)
+						filth["type"] = types
+						filth["text"] = texts
+						filth["merged_text"] = filth_obj.text
+						filth["merged"] = True
+					else: # filth_obj is Filth
+						filth["type"] = filth_obj.detector_name
+						filth["text"] = filth_obj.text
+						filth["merged"] = False
+					doc_filth.append(filth)
+
+				doc_filth_data = {}
+				doc_filth_data["url"] = doc["url"]
+				doc_filth_data["filth_count"] = doc_filth_count
+				doc_filth_data["word_count"] = doc_word_count
+				doc_filth_data["filth"] = doc_filth
+
+				# Clean
+				cleaned_text = scrubber.clean(doc_text)
+				doc["text"] = cleaned_text
+
+				docs_scrubbed.append(doc)
+				docs_filth_data.append(doc_filth_data)
+
+		file_filth_data = {}
+		file_filth_data["filename"] = filepath.split('/')[-1]
+		file_filth_data["filth_count"] = filth_count
+		file_filth_data["word_count"] = word_count
+		file_filth_data["filth_doc_count"] = filth_doc_count
+		file_filth_data["doc_count"] = len(lines)
+		file_filth_data["filth_data"] = docs_filth_data
+
+		return docs_scrubbed, file_filth_data
 
 
 def init_scrubber():
@@ -64,38 +127,39 @@ def init_scrubber():
 	return scrubber
 
 
-def save_to_file(data, out_dir, fname):
+def save_compressed_file(data, out_dir, filename):
 	if not os.path.exists(out_dir):
 		os.makedirs(out_dir)
-	fpath = os.path.join(out_dir, fname)
-	with open(fpath, 'w') as out_file:
+	filepath = os.path.join(out_dir, filename)
+	with xz.open(filepath, 'w') as out_file:
 		for x in data:
-			out_file.write(json.dumps(x) + "\n")
-	print(f"Written {len(data)} to {fpath}")
+			out_file.write((json.dumps(x) + "\n").encode("utf-8"))
 
 
-def save_pickle(data, out_dir, fname):
+def save_json_file(data, out_dir, filename):
 	if not os.path.exists(out_dir):
 		os.makedirs(out_dir)
-	fpath = os.path.join(out_dir, fname)
-	with open(fpath, 'wb') as out_file:
-		pickle.dump(data, out_file)
+	filepath = os.path.join(out_dir, filename)
+	with open(filepath, 'w') as out_file:
+		json.dump(data, out_file)
 
 
 def main():
-	filepaths = glob.glob(os.path.join(IN_DIR, "*.xz"))
+	filepaths_all = glob.glob(os.path.join(IN_DIR, "*.xz"))
+	filepaths_to_scrub = [filepath for filepath in filepaths_all if filepath.split('/')[-1] in TO_SCRUB]
 
 	scrubber = init_scrubber()
-	for filepath in filepaths:
-		docs_scrubbed, docs_filth = scrub(filepath, scrubber)
+	for filepath in filepaths_to_scrub:
+		docs_scrubbed, filth_data = scrub(filepath, scrubber)
 
-		fname = '.'.join((filepath.split('/')[-1]).split('.')[0:-1])
+		filename = filepath.split('/')[-1]
+		trunc_filename = ".".join(filename.split('.')[0:2])
 
-		# Save cleaned docs as jsonl
-		save_to_file(docs_scrubbed, OUT_DIR, fname)
+		# Save cleaned docs as compressed xz file
+		save_compressed_file(docs_scrubbed, OUT_DIR, filename)
 
-		# Pickle doc filth as a list of lists (filth list per doc)
-		save_pickle(docs_filth, FILTH_DIR, fname + ".pkl")
+		# Save file filth data to json
+		save_json_file(filth_data, FILTH_DIR, trunc_filename + ".json")
 
 
 if __name__ == '__main__':
